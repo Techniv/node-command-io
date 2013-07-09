dd34ee8284e1287be9b1f6d85eb33f5d0eecbb64
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2013-07-09 20:36:50 +0200

    Fix sandbox path resolve.

--- tests/commandio-sandbox.js
+++ tests/commandio-sandbox.js
@@ -1,12 +1,14 @@
 
 var sandbox = require('nodeunit').utils.sandbox;
+var path = require('path');
 var context = {
 	process: process,
 	module: {
 		exports: {}
 	},
 	require: require,
-	console: console
+	console: console,
+	__dirname: path.resolve(path.join(__dirname,'..','libs'))
 };
 context.global = context;
 
