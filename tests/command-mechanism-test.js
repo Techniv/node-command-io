58639191a6deaddf657dabea4269f3350277347e
Vincent Peybernes <vpeybernes.pro@gmail.com>
2013-07-14 01:36:24 +0200

    [TDD] create unit test to error management

--- tests/command-mechanism-test.js
+++ tests/command-mechanism-test.js
@@ -26,5 +26,79 @@ module.exports = {
 		commandio.addCommand(descriptor);
 		commandio.processCommand(['cmd']);
 		test.done();
+	},
+
+
+	/**
+	 * Test exception management.
+	 * Runtime never catch.
+	 * Errors are catch if their error lvl are >= exceptionCatchLvl parameter.
+	 * By default exceptionCatchLvl = 2 (3 for runtime).
+	 * @param test
+	 */
+	throwingException: function(test){
+		test.expect(4);
+		var descriptor;
+
+
+		descriptor = {
+			name: 'error',
+			description: 'cdm desc',
+			action: function(){
+				throw new this.CommandError('Error');
+			}
+		};
+
+		commandio.addCommand(descriptor);
+		test.throws(function(){
+			commandio.processCommand(['error']);
+		});
+
+
+		descriptor = {
+			name: 'error1',
+			description: 'cdm desc',
+			action: function(){
+				throw new this.CommandError('Notice', 1);
+			}
+		};
+
+		commandio.addCommand(descriptor);
+		test.doesNotThrow(function(){
+			commandio.processCommand(['error1']);
+		});
+
+
+		descriptor = {
+			name: 'error3',
+			description: 'cdm desc',
+			exceptionCatchLvl: 3,
+			action: function(){
+				throw new this.CommandError('Error');
+			}
+		};
+
+		commandio.addCommand(descriptor);
+		test.doesNotThrow(function(){
+			commandio.processCommand(['error3']);
+		});
+
+
+		descriptor = {
+			name: 'runtime',
+			description: 'cdm desc',
+			exceptionCatchLvl: 0,
+			action: function(){
+				throw new this.RuntimeCommandError('Runtime Error');
+			}
+		};
+
+		commandio.addCommand(descriptor);
+		test.throws(function(){
+			commandio.processCommand(['runtime']);
+		});
+
+
+		test.done();
 	}
 };
